f9fbe1008ad44cb6ef798e605b9eafe6a9edb15e
jsfreu <jsnels29@proton.me>
2024-06-21 20:48:33 +0200

    Last Stable

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,9 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\LeadsPanelProvider::class,
+    App\Providers\Filament\MembersPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
 ];
